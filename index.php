003ca805cbe3704dbd8b3449d4ea148006905eea
adrienCrosio <adrien.crosio@cpe.fr>
2020-10-15 16:54:38 +0200

    add .htaccess => entry point / create class Router

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 session_start();
 
 require('./libs/smarty/Smarty.class.php');
+require('./src/router/router.php');
 
 try {
     $bdd = new PDO("mysql:host=localhost;dbname=webapp", "adrien",  "crosio");
@@ -11,4 +12,8 @@ try {
     die();
 }
 
-echo("Ça marche !!!!!!");
+echo("Ça marche !!!!!!\n");
+// $string = "goto";
+$router = new Router();
+// $router->$string($_SERVER['REQUEST_URI']);
+$router->goto($_SERVER['REQUEST_URI']);
---
+++ src/router/router.php
@@ -0,0 +1,8 @@
+<?php
+
+class Router
+{
+    public function goto($uri){
+        echo("'".$uri."'");
+    }
+}
